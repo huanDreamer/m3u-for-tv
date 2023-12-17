4d130ae8764a112f502599efa013d70fd8528700
huan.dreamer@gmail.com <huan.dreamer@gmail.com>
2023-12-17 11:21:56 +0800

    fix bug

--- lib/tv_toast.dart
+++ lib/tv_toast.dart
@@ -5,7 +5,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 class TvToast {
   static void show(String message) {
     Fluttertoast.showToast(
-      msg: '发现新版本正在升级',
+      msg: message,
       toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 2,
